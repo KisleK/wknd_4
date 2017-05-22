79bb91feee1573e323867055ec1d9138f596347b
KisleK <kisle.kuhn1@gmail.com>
2017-05-22 14:53:53 -0500

    algo

--- algo/missing_letter.rb
+++ algo/missing_letter.rb
@@ -3,7 +3,6 @@
 # bonus: returns a string of all missing letters as a string. ex: find_missing_letter("ace") would return "bd", write your own test.
 
 def find_missing_letter(range)
-
     alphabet = ('a'..'z').to_a
     range_array = range.split('')
     first_letter = range_array.first
